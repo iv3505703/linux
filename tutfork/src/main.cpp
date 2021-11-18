d4731d5eede7b2e040261e976376fae4c1720841
Ivansquark <squark@mail.ru>
2021-11-18 20:03:25 +0300

    gets pid after fork()
    final commit

--- tutfork/src/main.cpp
+++ tutfork/src/main.cpp
@@ -3,9 +3,21 @@
 #include <cstdio>
 #include <stdlib.h>
 #include <pwd.h>
+#include <stdint.h>
 
 int main( int argc, char** argv) {
     int uid = getuid();
     printf("UID=%d", uid);
+
+    uint16_t InitPid = getpid();
+
+    pid_t result = fork();
+    if(result == -1) {
+        write(2, "Error\n", 7); return 1;
+    }
+    if(result == 0) printf("I am child PID=%d, PPID=%d\n", getpid(), getppid());
+    else printf("I am parent, result=%d, PID=%d, PPID=%d\n", result, getpid(), getppid());
+    
+    return 0;
 }
 
