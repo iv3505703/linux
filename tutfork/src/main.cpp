404a01bb3f11823218bf356be3a381eecf0afb63
Ivansquark <squark@mail.ru>
2021-11-18 20:49:19 +0300

    +-------------------------------------------------------+ | fork() + execve(char** PATH, char** argv, char** ENV) | +-------------------------------------------------------+

--- tutfork/src/main.cpp
+++ tutfork/src/main.cpp
@@ -5,19 +5,22 @@
 #include <pwd.h>
 #include <stdint.h>
 
+extern char** environ;
 int main( int argc, char** argv) {
+    uint16_t InitPid = getpid();
     int uid = getuid();
     printf("UID=%d", uid);
-
-    uint16_t InitPid = getpid();
 
     pid_t result = fork();
     if(result == -1) {
         write(2, "Error\n", 7); return 1;
     }
-    if(result == 0) printf("I am child PID=%d, PPID=%d\n", getpid(), getppid());
+    if(result == 0) {
+        printf("I am child process PID=%d, PPID=%d\n", getpid(), getppid());
+        char* argv[] = {"ps", "-f", NULL}; 
+        execve("/bin/ps", argv, environ);
+    }
     else printf("I am parent, result=%d, PID=%d, PPID=%d\n", result, getpid(), getppid());
-    
+    sleep(10);
     return 0;
 }
-
