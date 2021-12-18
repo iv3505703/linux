50c7f4d896d2de9665f6653bf6c3d8c617bf83a4
iv3505703 <iv3505703@gmail.com>
2021-12-18 23:38:11 +0300

    +ccls

---
+++ tutgit/inc/main.h
@@ -0,0 +1,6 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+int sum(int x, int y);
+
+#endif //MAIN_H
--- tutgit/src/main.cpp
+++ tutgit/src/main.cpp
@@ -1,8 +1,19 @@
 #include <unistd.h>
+#include "stdio.h"
+
+#include "main.h"
+
 
 int main(int argc, char** argv) {
     write(1, "opa\n",4);
     write(2, "error\n", 6);
     write(1, "dev1\n", 4);
+    char* str = "\x31 \x32";
+    printf("\n%s\n", str);
+    printf("\nsum=%d",sum(5,6));
     return 0;
+}
+
+int sum(int x, int y) {
+    return x + y;
 }
