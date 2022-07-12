ffda63d3e7897662d0050729bdf6b42bd9c49bae
Ivansquark <squark@mail.ru>
2022-07-12 16:49:26 +0300

    +no safe script w/o sudo

---
+++ script_wo_sudo_unsafe/start.c
@@ -0,0 +1,14 @@
+#include <unistd.h>
+#include <stdio.h>
+
+int main() {
+    setuid(0);
+    //system("./script");
+    pid_t num = fork();
+    if(!num) {
+        _exit(0);
+    } else if(num != -1){
+        execl("./script", NULL);
+    } else printf("ERROR");
+    return 0;
+}
